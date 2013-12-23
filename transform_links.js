32d3ef4138e969a80c0d70dbeecf032fdfae2172
Andrew Moffat <andrew.robert.moffat@gmail.com>
2013-12-23 16:49:00 -0600

    if url is absent, use contents of a tag

--- transform_links.js
+++ transform_links.js
@@ -1,7 +1,12 @@
 $(function() {
    $.each($("a[href^='#://']"), function(i, el) {
        el = $(el);
-       var original = el.attr("href");
+       var original = el.attr("href").substr(4);
+       
+       if (original == "") {
+           original = el.text();
+       }
+       
        var hash = hashIt(original);
        el.attr("href", hash);
        el.attr("data-original", original);
